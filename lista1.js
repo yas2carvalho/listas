050913dc5304f75e966d892180e27ec2f3fab1a7
yas2carvalho <yasmim.c.silva16@aluno.senai.br>
2025-01-29 16:28:49 -0300

    Initial commit

---
+++ lista1.js
@@ -0,0 +1,50 @@
+//01
+
+let z = 10
+let x = 5
+
+let soma = z + x
+let sub = z - x
+let mult = z * x
+let div = z / x
+let resto = z % x
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+
+//02
+
+let a = 10
+let b = 5
+let area = a*l
+console.log(area)
+
+//03
+
+let c = 10
+
+let dobro = c*2
+let metade = c/2
+
+console.log(dobro)
+console.log(metade)
+
+
+//04
+
+let d = 2
+let e = 4
+let f = 6
+
+let media = (d + e + f) / 3
+console.log(media)
+
+//05
+
+let celsius = 20
+
+let fahrenheit = (celsius * 9/5) + 32
+console.log(fahrenheit)
---
+++ lista2.js
@@ -0,0 +1,19 @@
+//01
+let numero = 10
+let numero2 = 5
+
+console.log(numero > numero2)
+console.log(numero <= numero2)
+console.log(numero == numero2)
+
+//02
+let idade = 18
+console.log(idade >= 16)
+
+//03
+let idade2 = 26
+console.log(idade2 >= 18)
+console.log( idade2 >= 60)
+
+//04
+
